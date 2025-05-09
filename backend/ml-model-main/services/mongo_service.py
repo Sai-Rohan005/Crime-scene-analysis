3d0f01c5936bf55bb25abbbf01d6ab91ab79b446
Sai Rohan <sairohan005@gmail.com>
2025-05-09 18:19:44 +0530

    some functions added

--- backend/ml-model-main/services/mongo_service.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from config.settings import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client["forensic_ai"]
-summaries_collection = db["image_summaries"]
-reports_collection = db["case_reports"]
